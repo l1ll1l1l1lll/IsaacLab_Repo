033ce114a40bd803eec09e5dd1065e69c209e6a5
l1ll1l1l1lll <yuhao_CAO@outlook.com>
2025-12-07 21:57:28 +0100

    init

---
+++ test/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"C:/isaac-lab/IsaacLab/isaaclab.bat" -p "C:/isaac-lab/IsaacLab/scripts/tutorials/00_sim/create_empty.py"
---
+++ test/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def add_arrays(arr1, arr2):
+    return np.add(arr1, arr2)
